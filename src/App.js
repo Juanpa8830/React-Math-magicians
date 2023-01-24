d942508bb540e27036a4229439b328d601b2748a
Juanpa8830 <jp3_4@hotmail.com>
2023-01-23 20:10:25 -0500

    Configure react-router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
 import Calculator from './Components/Calculator';
+import HomeSection from './pages/home';
+import Quote from './pages/quote';
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="calc-container">
-        <Calculator />
-      </div>
-    </div>
+
+    <>
+      <nav className="nav">
+        <h1 className="navtitle">Math Magicians</h1>
+        <ul className="navlist">
+          <li className="listItem"><Link className="listItem" to="/">Home</Link></li>
+          <li className="listItem"><Link to="/Calculator">Calculator</Link></li>
+          <li className="listItem"><Link to="/Quote">Quote</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomeSection />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/Quote" element={<Quote />} />
+
+      </Routes>
+    </>
   );
 }
